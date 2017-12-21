0a63616b9b082d109914074241a48d8aeb5547d3
Vegard Nossum <vegard.nossum@gmail.com>
2017-12-21 11:29:35 +0100

    Initial version
    Add a Python script that can chop the original frames into 256 8x8 tiles.
    
    This uses a bunch of heuristics to try to produce something that is visually
    pleasing and not too far from the original while staying within the limits
    of the GameBoy (assuming all tiles are loaded at all times; if we assume that
    some tiles are only used in certain frames and that you can modify those
    tiles from frame to frame, you can squeeze more out a higher number of
    different tiles).

---
+++ autotile.py
@@ -0,0 +1,169 @@
+import os
+import random
+import sys
+
+from glob import glob
+from PIL import Image
+
+# frame size (after downscaling)
+size = (180, 170)
+
+xoff = 0
+yoff = 3
+
+frames = []
+for filename in sorted(glob('frames/*.gif.*')):
+    im = Image.open(filename)
+    im = im.resize(size, Image.NEAREST)
+
+    frames.append(im.crop((xoff, yoff, xoff + 8 * ((size[0] - xoff) // 8), yoff + 8 * ((size[1] - yoff) // 8))))
+
+tiles = {}
+tile_ims = {}
+immutable = set()
+
+for i, frame in enumerate(frames):
+    w, h = frame.size
+
+    for y in range(0, h, 8):
+        for x in range(0, w, 8):
+            tile = frame.crop((x, y, x + 8, y + 8))
+            #tile.save('tiles/%04u.png' % len(tiles))
+            t = tuple(tile.getdata())
+            tiles[t] = tiles.get(t, 0) + 1
+            tile_ims[t] = tile
+
+            #if i == 0 and x == 8 * 8 and y <= 6 * 8:
+            if x in [56, 64]:
+                immutable.add(t)
+
+            #if x == 0 and y == 64:
+            #    print t
+
+def entropy(t):
+    # this just counts the number of distinct colors for now... quite pathetic
+
+    c = {}
+    for p in t:
+        c[p] = c.get(p, 0) + 1
+
+    # the fewer distinct colors, the harder this tile should be to collapse
+    return 100 * (4 - len(c))
+
+# try to reduce the number of tiles by collapsing the two most similar tiles into one
+
+def delta(t1, t2):
+    return sum(abs(x - y) for x, y in zip(t1, t2))
+
+def dither(i, a, b):
+    #return (a + b) // 2
+
+    # https://en.wikipedia.org/wiki/Ordered_dithering
+    matrix = [
+         0, 48, 12, 60,  3, 51, 15, 63,
+        32, 16, 44, 28, 35, 19, 47, 31,
+         8, 56,  4, 52, 11, 59,  7, 55,
+        40, 24, 36, 20, 43, 27, 39, 23,
+         2, 50, 14, 62,  1, 49, 13, 61,
+        34, 18, 46, 30, 33, 17, 45, 29,
+        10, 58,  6, 54,  9, 57,  5, 53,
+        42, 26, 38, 22, 41, 25, 37, 21,
+    ]
+
+    if a == b:
+        return a
+
+    a, b = sorted((a, b))
+    if a + 2 == b:
+        return a + 1
+
+    if a + 3 == b:
+        a = a + 1
+        b = b - 1
+
+    #if a + .5 < matrix[i] / 63.:
+    #    return a
+    #else:
+    #    return b
+
+    if random.random() < matrix[i] / 64.:
+        return a
+    else:
+        return b
+
+    # in the end, this looks better to me
+    #return random.choice((a, b))
+
+def collapse_similar(tiles):
+    min_d = None
+
+    #print immutable
+
+    for i, (t1, n1, c1) in enumerate(tiles):
+        if t1 in immutable:
+            continue
+
+        for ioff, (t2, n2, c2) in enumerate(tiles[i + 1:]):
+            if t2 in immutable:
+                continue
+
+            #if t1 in immutable and t2 in immutable:
+            #    continue
+
+            j = i + ioff + 1
+            d = (n1 + n2) * (delta(t1, t2) + c1 + c2)
+            if min_d is None or d < min_d:
+                min_d = d
+                min_i = i
+                min_j = j
+
+    # remove the larger index first so the first remains valid
+    assert min_i < min_j, (min_i, min_j)
+    t2, n2, c2 = tiles.pop(min_j)
+    t1, n1, c1 = tiles.pop(min_i)
+    print min_d, n1, n2, c1, c2
+
+    # pick the one with fewer modifications, if possible
+    if c1 < c2 or t1 in immutable:
+        tiles.append((t1, n1 + n2, max(c1, c2) + 1))
+        tile_ims[t2] = tile_ims[t1]
+    elif c2 < c1 or t2 in immutable:
+        tiles.append((t2, n1 + n2, max(c1, c2) + 1))
+        tile_ims[t1] = tile_ims[t2]
+    else:
+        #t3 = tuple((x + y) // 2 for x, y in zip(t1, t2))
+        t3 = tuple(dither(i, x, y) for i, (x, y) in enumerate(zip(t1, t2)))
+        print t3
+        im = tile_ims[t1].copy()
+        im.putdata(list(t3))
+        tile_ims[t3] = im
+
+        #t3 = random.choice((t1, t2))
+        #if sum(t1) < sum(t2):
+        #    t3 = t2
+        #else:
+        #    t3 = t1
+
+        tiles.append((t3, n1 + n2, max(c1, c2) + 1))
+        tile_ims[t1] = tile_ims[t3]
+        tile_ims[t2] = tile_ims[t3]
+
+    return tiles
+
+tiles = [(t, n, entropy(t)) for t, n in tiles.iteritems()]
+
+while len(tiles) > 256:
+    tiles = collapse_similar(tiles)
+    print len(tiles)
+
+for i, frame in enumerate(frames):
+    w, h = frame.size
+
+    for y in range(0, h, 8):
+        for x in range(0, w, 8):
+            tile = frame.crop((x, y, x + 8, y + 8))
+            t = tuple(tile.getdata())
+            tile = tile_ims[t]
+            frame.paste(tile, (x, y))
+
+    frame.save('output/%04u.gif' % i)
