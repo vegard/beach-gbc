c75ba51d616d21d1230b8f1613c6a00caba65ef3
Vegard Nossum <vegard.nossum@gmail.com>
2017-12-22 21:18:58 +0100

    Output beach.gbc instead of main.gb

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 rgbds/rgbasm -o main.o main.asm
-rgbds/rgblink -m main.map -o main.gb main.o
-rgbds/rgbfix -v -p 0 main.gb
+rgbds/rgblink -m main.map -o beach.gbc main.o
+rgbds/rgbfix -v -p 0 beach.gbc
